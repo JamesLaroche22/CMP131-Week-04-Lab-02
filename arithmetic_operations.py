c0c9c90a450346f4b7087bd01140a7eaad7e7a58
JamesLaroche22 <laroche.james@student.ccm.edu>
2026-09-23 11:22:48 -0400

    Implement basic arithmetic operations in Python

---
+++ arithmetic_operations.py
@@ -0,0 +1,27 @@
+#James_Laroche
+#CMP-131 
+#Week_4 
+#Lab_2
+#Arithmetic_Operation 
+#September_23rd 
+num1=float(input("Enter Value #1: "))
+num2=float(input("Enter Value #2: ")) 
+print()
+addition= num1 + num2 #Equation for addition. 
+subtraction=num1 - num2 #equation for subtraction.
+division=num1/num2 #Equation for division.
+multiplication= num1 * num2 #Equation for multiplication.
+power=num1**num2 #Equation to raise the first number to the power of the second number.
+average= (num1 + num2) / 2 #Equation for the average of the two values given.
+print(f"Addition Result (+) : {num1} + {num2} = {addition:,.2f}")
+print()
+print(f"Subtraction Result (-): {num1} - {num2} = {subtraction:,.2f}") 
+print()
+print(f"Multiplication Result (x): {num1} x {num2} = {multiplication:,.2f}") 
+print()
+print(f"Division Result (÷): {num1} ÷ {num2} = {division:,.2f}") 
+print()
+print(f"Power Result (^): {num1} ^ {num2} =  {power:,.2f}")  
+print()
+print(f"Average Result (μ): ({num1} + {num2}) ÷ 2 = {average:,.2f}") 
+      
